dfcf48cf67219de588602d1152fd74256ea51fdf
milkshake4life <elee91214@gmail.com>
2023-05-27 17:49:27 -0700

    most of the front end finished

--- main.py
+++ main.py
@@ -1,68 +1,46 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from firebase import firebase
 
-class MainApp(App):
+Window.size = (310, 580)
+
+#Initialize Firebase
+
+
+class MainApp(MDApp):
     def build(self):
-        self.operators = ["/", "*", "+", "-"]
-        self.last_was_operator = None
-        self.last_button = None
-
-        main_layout = BoxLayout(orientation = "vertical")
-        self.solution = TextInput(background_color = "black", foreground_color = "white")
-
-        main_layout.add_widget(self.solution)
-        buttons = [
-            ["7", "8", "9", "/"],
-            ["4", "5", "6", "*"],
-            ["7", "8", "9", "+"],
-            [".", "0", "C", "-"],
-        ]
-
-        for row in buttons:
-            h_layout = BoxLayout()
-            for label in row:
-                button = Button(
-                    text = label, font_size = 30, background_color = "grey",
-                    pos_hint = {"center_x": 0.5, "center_y": 0.5},
-                )
-                button.bind(on_pressed = self.on_button_pressed)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(Builder.load_file("main.kv"))
+        screen_manager.add_widget(Builder.load_file("login.kv"))
+        screen_manager.add_widget(Builder.load_file("signup.kv"))
+        screen_manager.add_widget(Builder.load_file("regional.kv"))
+        screen_manager.add_widget(Builder.load_file("scout1.kv"))
+        screen_manager.add_widget(Builder.load_file("scout2.kv"))
+        screen_manager.add_widget(Builder.load_file("s1s.kv"))
+        screen_manager.add_widget(Builder.load_file("s1i.kv"))
+        screen_manager.add_widget(Builder.load_file("s2s.kv"))
+        screen_manager.add_widget(Builder.load_file("s2i.kv"))
         
-        equal_button = Button(
-            text = "=", font_size = 30, background_color = "grey",
-            pos_hint = {"center_x": 0.5, "center_y": 0.5}
-        )
-        equal_button.bind(on_pressed = self.on_solution)
-        main_layout.add_widget(equal_button)
 
-        return main_layout
+        return screen_manager
+    
+    def send_data(self, email, password):
+        firebase = firebase.FirebaseApplication('https://scouting-app-68229-default-rtdb.firebaseio.com/', None)
 
-    def on_button_pressed(self, instance):
-        current = self.solution.text
-        button_text = instance.text
-        if button_text ==  "C":
-            self.solution.text = ""
-        else:
-            if current and (self.last_was_operator and button_text in self.operators):\
-                return 
-            elif current == "" and button_text in self.operators:
-                return
-            else:
-                new_text = current + button_text
-                self.solution.text = new_text
-        self.last_button = button_text
-        self.last_was_operator = self.last_button in self.operators
+        data = {
+            'Team Number': email,
+            'Password': password
+        }
 
-    def on_solution(self, instance):
-        text = self.solution.text
-        if text:
-            solution = str(eval(self.solution.text))
-            self.solution.text = solution
-
+        # firebase.post('https://scouting-app-68229-default-rtdb.firebaseio.com/Users', data)
 
 if __name__ == "__main__":
+    LabelBase.register(name="MPoppins", fn_regular="C:\\Users\\elee9\\Downloads\\Poppins\\Poppins-Medium.ttf")
+    LabelBase.register(name="BPoppins", fn_regular="C:\\Users\\elee9\\Downloads\\Poppins\\Poppins-SemiBold.ttf")
+
+
     app = MainApp()
     app.run()
