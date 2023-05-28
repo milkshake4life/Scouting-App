5aa66a84a1165463fd3d758c16bb82bc2fb934c8
milkshake4life <45021944+milkshake4life@users.noreply.github.com>
2023-05-27 19:32:51 -0700

    correct login

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
-from firebase import firebase
+
 
 Window.size = (310, 580)
-
-#Initialize Firebase
 
 
 class MainApp(MDApp):
@@ -23,19 +21,47 @@ class MainApp(MDApp):
         screen_manager.add_widget(Builder.load_file("s1i.kv"))
         screen_manager.add_widget(Builder.load_file("s2s.kv"))
         screen_manager.add_widget(Builder.load_file("s2i.kv"))
+        screen_manager.add_widget(Builder.load_file("newAccount.kv"))
         
 
         return screen_manager
     
-    def send_data(self, email, password):
+    def sendData(self, team_number_input, password_input):
+        from firebase import firebase
         firebase = firebase.FirebaseApplication('https://scouting-app-68229-default-rtdb.firebaseio.com/', None)
 
+        team_number_text = team_number_input.text
+        password_text = password_input.text
+
         data = {
-            'Team Number': email,
-            'Password': password
+            'Team Number': team_number_text,
+            'Password': password_text
         }
 
-        # firebase.post('https://scouting-app-68229-default-rtdb.firebaseio.com/Users', data)
+        firebase.post('https://scouting-app-68229-default-rtdb.firebaseio.com/Users', data)
+        
+    def verifyData(self, team_number_input, password_input):
+        from firebase import firebase
+        firebase = firebase.FirebaseApplication('https://scouting-app-68229-default-rtdb.firebaseio.com/', None)
+
+        team_number_text = team_number_input.text
+        password_text = password_input.text
+
+        is_verified = False
+
+        result = firebase.get('https://scouting-app-68229-default-rtdb.firebaseio.com/Users', '')
+
+        for i in result.keys():
+            if result[i]['Team Number'] == team_number_text:
+                if result[i]['Password'] == password_text:
+                    self.root.get_screen('login').ids.status_label.text = ""
+                    is_verified = True
+                    self.root.transition.direction = "left"
+                    self.root.current = "regional"
+                    break
+    
+        if not is_verified:
+            self.root.get_screen('login').ids.status_label.text = "Incorrect team number or password"
 
 if __name__ == "__main__":
     LabelBase.register(name="MPoppins", fn_regular="C:\\Users\\elee9\\Downloads\\Poppins\\Poppins-Medium.ttf")
